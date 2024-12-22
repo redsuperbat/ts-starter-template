f8d3ebe12cb17beae11680072d4ac9a37979330d
Max Netterberg <max.netterberg@gmail.com>
2024-12-22 15:07:40 +0100

    remove more opinions

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -11,11 +11,6 @@ export default defineConfig({
         changeOrigin: true,
         secure: false,
       },
-      "/session": {
-        target: "http://localhost:3001",
-        changeOrigin: true,
-        secure: false,
-      },
     },
   },
 });
